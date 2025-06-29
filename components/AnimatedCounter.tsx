de733eb02a852f8c96b082c2515c61be0ecc5638
Evant Valery Wijaya <valeryevantq@gmail.com>
2025-06-29 12:40:37 +0700

    fix (ui) : change decimal into . from ,

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -4,7 +4,7 @@ import CountUp from "react-countup";
 const AnimatedCounter = ({ value }: { value: number }) => {
   return (
     <div className="w-full">
-      <CountUp end={value} prefix="$" decimal="," decimals={2} />;
+      <CountUp end={value} prefix="$" decimals={2} />;
     </div>
   );
 };
